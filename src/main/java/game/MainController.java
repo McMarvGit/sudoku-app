76de7dc5e7556696082fe9eb6f15ab4f9af5b896
Marvin Frommhagen <mfrommha@mail.uni-mannheim.de>
2024-10-13 15:24:29 +0200

    comment removed

--- src/main/java/game/MainController.java
+++ src/main/java/game/MainController.java
@@ -15,7 +15,6 @@ import javafx.stage.Stage;
 import java.util.Locale;
 
 public class MainController extends Application {
-//Test commit 
     private Stage mainStage;
 
     private MainSceneController mainSceneController;
